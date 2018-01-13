5b2c7e3df241b0967804a319f1ce149964011887
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-01-13 21:53:57 +0100

    Remove too specific type declarations from tests

--- test/AVLTreeTest/ParametricTests.hs
+++ test/AVLTreeTest/ParametricTests.hs
@@ -16,48 +16,6 @@ instance (Ord a, Arbitrary a, Arbitrary b) => Arbitrary (AVLTree a b) where
             return $ (foldr (&:) newTree (zip keys vals))
         )
 
---instance (Ord a, Arbitrary b) => Arbitrary OrdTree a b where
---instance Arbitrary (AVLTree Int b) where
---    arbitrary = sized $ (\n -> do
---            keys <- vector n :: Gen [a]
---            vals <- vector n :: Gen [b]
---            return $ OrdTree $ (foldr (&:) newTree (zip keys vals))
---        )
---        --return $ foldr (&:) newTree xs
---
-newtype IntTree  = IntTree (AVLTree Int Int) deriving Show
-
-newtype Tree2 b = Tree2 (AVLTree Int b) deriving Show
-
---instance (Arbitrary b) => Arbitrary (Tree2 b) where
---    --spec arbitrary :: Gen IntTree
---    arbitrary = sized $ (\n -> do
---            keys <- vector n -- :: Gen [Int]
---            vals <- vector n -- :: Gen [b]
---            return $ Tree2 (foldr (&:) newTree (zip keys vals)))
-
-
-instance Arbitrary (IntTree) where
-    --spec arbitrary :: Gen IntTree
-    arbitrary = sized $ (\n -> do
-        --xs <- arbitrary :: Gen [(a, b)]
-            keys <- vector n :: Gen [Int]
-            vals <- vector n :: Gen [Int]
-            --vals <- vector n arbitrary
-            return $ IntTree (foldr (&:) newTree (zip keys vals))
-        )
-        --return $ foldr (&:) newTree xs
-
---instance (Arbitrary b) => Arbitrary (IntTree b) where
---    --spec arbitrary :: Gen IntTree
---    arbitrary = sized $ (\n -> do
---        --xs <- arbitrary :: Gen [(a, b)]
---            keys <- vector n :: Gen [Int]
---            vals <- vector n :: Gen [b]
---            --vals <- vector n arbitrary
---            return $ IntTree (foldr (&:) newTree (zip keys vals))
---        )
---        --return $ foldr (&:) newTree xs
 
 prop_insert_imdepotence k v =
     (insert k v $ insert k v $ newTree) == (insert k v $ newTree)
