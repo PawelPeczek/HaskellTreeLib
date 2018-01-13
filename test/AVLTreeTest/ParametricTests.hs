375a40793444bcfeddea6235988ba62c51e45eae
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-01-13 23:21:38 +0100

    Add quickcheck test for linearKeysOrder

--- test/AVLTreeTest/ParametricTests.hs
+++ test/AVLTreeTest/ParametricTests.hs
@@ -5,23 +5,26 @@ import Test.QuickCheck.All
 import Test.QuickCheck
 import AVLTree
 
-prop_foo x y = x + y == y + x
-
 --newtype (Ord a) => OrdTree a b = OrdTree (AVLTree a b)
 
 instance (Ord a, Arbitrary a, Arbitrary b) => Arbitrary (AVLTree a b) where
     arbitrary = sized $ (\n -> do
-            keys <- vector n -- :: Gen [a]
-            vals <- vector n -- :: Gen [b]
-            return $ (foldr (&:) newTree (zip keys vals))
+            pairs <- vector n
+            return $ foldr (&:) newTree pairs
         )
+
+isOrdered :: (Ord a) => [a] -> Bool
+isOrdered [] = True
+isOrdered [_] = True
+isOrdered (x:y:xs) = x <= y && isOrdered xs
 
 prop_singletonInsert_imdepotence k v t =
     classify (t == newTree) "empty tree" $
     (insertSingleton k v $ insertSingleton k v t) == (insertSingleton k v t)
 
-prop_insertGeneratesValidTree :: (Ord a) => [(a, b)] -> Bool
-prop_insertGeneratesValidTree xs = AVLTree.isValid $ AVLTree.fromList xs
+prop_insertGeneratesValidTree = AVLTree.isValid . AVLTree.fromList
+
+prop_linearOrderIsOrdered = isOrdered . linearKeysOrder 
 
 --------------------------
 return []
